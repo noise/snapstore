de9f6b3128a75d484ef4661461354005506076c9
Bret Barker <bret.barker@canonical.com>
2016-06-23 17:06:26 -0400

    minor cleanup

--- store.py
+++ store.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request, url_for, send_from_directory
+from flask import Flask, Response, request, send_from_directory
 import sys
 
 FILES = 'files'
@@ -7,12 +7,12 @@ FILES = 'files'
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello basic snap store!"
+    return 'Hello basic snap store!'
 
 
-@app.route("/api/v1/search")
+@app.route('/api/v1/search')
 def search():
     ''' note in 2.0.9 snapd still uses search for package details
     before install as well as for find '''
@@ -27,15 +27,14 @@ def search():
         with open(FILES + '/' + name + '.meta', 'r') as meta:
             return Response(meta.read(), mimetype='application/hal+json')
     except Exception as e:
-        print e
         return Response('{}', mimetype='application/hal+json')
 
 
-@app.route("/anon/download-snap/<name>")
+@app.route('/anon/download-snap/<name>')
 def download(name):
     # TODO: sanitize names
     return send_from_directory(FILES, name)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
