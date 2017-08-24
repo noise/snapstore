99eb6c1afb66f4455873d45e35532ffe6dd1e3a2
bret barker <bret@abitrandom.net>
2017-08-24 14:50:14 -0400

    remove

--- store-wrapper.sh
+++
@@ -1,8 +0,0 @@
-DATA_DIR=$SNAP_DATA
-if [ ! -d "$DATA_DIR/files" ]; then
-    echo "Configuring snapstore-example..."
-    cp -r $SNAP/files $DATA_DIR
-fi
-    
-FILES=$DATA_DIR/files store.py
-    
